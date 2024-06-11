ebc5c3f118139ea979048deac2b5995f6396859c
CodeBy_HP <code.by.hp@gmail.com>
2024-06-11 18:22:03 +0530

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://happy-swift-51.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+export default authConfig;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lovely-flamingo-139.convex.cloud",
+      },
+      {
+        protocol: 'https',
+        hostname: 'diligent-mink-234.convex.cloud',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+      },
+      
+    ],
+  },
+};
 
 export default nextConfig;
